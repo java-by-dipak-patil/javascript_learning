13935e609302688a4d057924849f5869a4958238
dipakhiwarkar <dipakhiwarkar96@gmail.com>
2025-01-24 00:41:25 +0530

    first commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId=1233;//value cannot be changed
+const accountName="saving";
+let email="d@gmail.com";//
+var accCity="Jaipur";
+accCountry="india";
+
+//accountId=5;
+email="d@hotmail.com"
+accCity="bangluru"
+console.log(accountId);
+console.table([accountId,email,accCity,accCountry])
